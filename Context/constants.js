6829e9a1418d09d1f46ff15678bf93c3f3182dbc
Shashank Dev <shashankd459@gmail.com>
2025-10-31 18:00:04 +0530

    git updated constants.js

--- Context/constants.js
+++ Context/constants.js
@@ -1,5 +1,24 @@
+import booToken from "./BooToken.json";
+import lifeToken from "./LifeToken.json";
+import singleSwapToken from "./SingleSwapToken.json";
+import swapMultiHop from "./SwapMultiHop.json";
 
+// BOO TOKEN
+export const BooTokenAddress = "0x738823e368C80622Bb840D741BC707de4c7b5212";
+export const BooTokenABI = booToken.abi;
 
+// LIFE TOKEN
+export const LifeTokenAddress = "0x7816d99063dfEc136d2139681D3fbD92375Ec14c";
+export const LifeTokenABI = lifeToken.abi;
+
+// SingleSwapToken
+export const SingleSwapTokenAddress =
+  "0xf3800C1f5374d9104ab20A48F1F83485BD9a37da";
+export const SingleSwapTokenABI = singleSwapToken.abi;
+
+// SwapMultiHop
+export const SwapMultiHopAddress = "0x0222F49C69567eB50f425AF01E2F9FCc451e807B";
+export const SwapMultiHopABI = swapMultiHop.abi;
 
 // Boo deployed to 0x738823e368C80622Bb840D741BC707de4c7b5212
 // Life deployed to 0x7816d99063dfEc136d2139681D3fbD92375Ec14c
